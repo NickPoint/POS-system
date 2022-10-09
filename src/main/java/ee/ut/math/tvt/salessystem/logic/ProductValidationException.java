edb6935e103fc439f027aee09a5bf1b47fd908ed
Artur Aleksander Kanosin <artur.aleksander.kanosin@ut.ee>
2022-10-10 02:51:31 +0300

    LPM-51 #comment exception is added

---
+++ src/main/java/ee/ut/math/tvt/salessystem/logic/ProductValidationException.java
@@ -0,0 +1,10 @@
+package ee.ut.math.tvt.salessystem.logic;
+
+import ee.ut.math.tvt.salessystem.SalesSystemException;
+
+public class ProductValidationException extends SalesSystemException {
+
+    public ProductValidationException(String msg) {
+
+    }
+}
