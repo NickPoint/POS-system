f08afe4739a557dfef51c44a02b70844f9e2bc40
Artur Aleksander Kanosin <artur.aleksander.kanosin@ut.ee>
2022-10-28 01:53:10 +0300

    LPM-60 #comment Functionality is implemented  #done

---
+++ src/main/java/ee/ut/math/tvt/salessystem/dataobjects/Purchase.java
@@ -0,0 +1,2 @@
+package ee.ut.math.tvt.salessystem.dataobjects;public class Purchase {
+}
--- src/main/java/ee/ut/math/tvt/salessystem/logic/History.java
+++ src/main/java/ee/ut/math/tvt/salessystem/logic/History.java
@@ -1,4 +1,47 @@
 package ee.ut.math.tvt.salessystem.logic;
 
+import ee.ut.math.tvt.salessystem.SalesSystemException;
+import ee.ut.math.tvt.salessystem.dao.SalesSystemDAO;
+import ee.ut.math.tvt.salessystem.dataobjects.Purchase;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 public class History {
+    private final SalesSystemDAO dao;
+
+    private static final Logger log = LogManager.getLogger(History.class);
+
+    public History(SalesSystemDAO dao) {
+        this.dao = dao;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public List<Purchase> getLastTenPurchases(){
+        List<Purchase> purchases = dao.getPurchases();
+        return purchases.subList(purchases.size()  - 10, purchases.size());
+    }
+
+
+
+    /**
+     *
+     * @param p
+     * @return
+     */
+    private List<Purchase> getWithPredicate(Predicate<Purchase> p){
+        return dao.getPurchases()
+                .stream()
+                .filter(p::test)
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
 }
