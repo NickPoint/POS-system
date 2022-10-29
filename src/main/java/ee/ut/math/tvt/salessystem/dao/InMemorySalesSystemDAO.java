c908a3bb5f02596b0f07ccdae215fd79565ec868
Artur Aleksander Kanosin <artur.aleksander.kanosin@ut.ee>
2022-10-29 20:52:13 +0300

    LPM-60 #comment made method fool-proof added dummy history generation

--- src/main/java/ee/ut/math/tvt/salessystem/dao/InMemorySalesSystemDAO.java
+++ src/main/java/ee/ut/math/tvt/salessystem/dao/InMemorySalesSystemDAO.java
@@ -6,8 +6,10 @@ import ee.ut.math.tvt.salessystem.dataobjects.StockItem;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class InMemorySalesSystemDAO implements SalesSystemDAO {
 
@@ -32,7 +34,62 @@ public class InMemorySalesSystemDAO implements SalesSystemDAO {
         this.stockItemList = items;
         this.soldItemList = new ArrayList<>();
         this.purchaseList = new ArrayList<>();
+        items.add(new StockItem(1L, "Lays chips", 11.0, 5));
+        items.add(new StockItem(2L, "Chupa-chups", 8.0, 8));
+        items.add(new StockItem(3L, "Frankfurters", 15.0, 12));
+        items.add(new StockItem(4L, "Free Beer", 0.0, 100));
+        items.add(new StockItem(5L, "Sussy baka", 3.0, 91));
+        items.add(new StockItem(6L, "Los pollos hermanos", 80, 1));
+        items.add(new StockItem(7L, "Aboltus", 7.0, 1));
+        items.add(new StockItem(8L, "我们爱茶", 3.5, 8));
+        items.add(new StockItem(9L, "Eboldas", 0.0, 1));
+        items.add(new StockItem(10L, "Kebab", 6.90, 3));
+        items.add(new StockItem(11L, "Noodles", 5.50, 2));
+        items.add(new StockItem(12L, "Sirka dish", 11.0, 3));
+        items.add(new StockItem(13L, "Lasagne", 5.80, 4));
+        items.add(new StockItem(14L, "Mao pao chicken", 6.50, 1));
+        items.add(new StockItem(15L, "我爱咖啡", 12, 4));
+        generateHistory();
     }
+
+
+    private void generateHistory() {
+        int currentYear = LocalDate.now().getYear();
+        //Majority is more than a year old
+        generateHistory(currentYear, 3, 1, 12);
+        //Majority is up to a year old
+        generateHistory(currentYear, 1, 0, 7);
+    }
+
+    /**
+     * Generate dummy purchase history
+     * @param year a year around which we build an interval
+     * @param deltaS defines start of the date interval
+     * @param deltaE defines end of the date interval
+     * @param n number of purchases to add
+     */
+    private void generateHistory(int year, int deltaS, int deltaE, int n) {
+        var current = ThreadLocalRandom.current();
+        for (int i = 0; i < n; i++) {
+            var date = LocalDate.of(
+                    current.nextInt(year - deltaS, year - deltaE+1),
+                    current.nextInt(1, 13),
+                    current.nextInt(1, 29)
+            );
+            var time = LocalTime.of(
+                    current.nextInt(0, 24),
+                    current.nextInt(0, 60)
+            );
+            List<SoldItem> purchase = new ArrayList<>();
+            int numberOfProducts = current.nextInt(1, 15);
+            for (int j = 0; j < numberOfProducts; j++) {
+                int choice = current.nextInt(0, stockItemList.size());
+                purchase.add(new SoldItem(stockItemList.get(choice), current.nextInt(1, 33)));
+            }
+            purchaseList.add(new Purchase(purchase, time, date));
+        }
+    }
+
 
     @Override
     public List<StockItem> findStockItems() {
@@ -60,8 +117,7 @@ public class InMemorySalesSystemDAO implements SalesSystemDAO {
     }
 
 
-
-    public void finalisePurchase(){
+    public void finalisePurchase() {
         purchaseList.add(new Purchase(soldItemList, LocalTime.now(), LocalDate.now()));
         soldItemList.clear();
     }
@@ -69,7 +125,7 @@ public class InMemorySalesSystemDAO implements SalesSystemDAO {
     @Override
     public void saveStockItem(StockItem stockItem) {
         for (int i = 0; i < stockItemList.size(); i++) {
-            if(stockItemList.get(i).getId().equals(stockItem.getId())){
+            if (stockItemList.get(i).getId().equals(stockItem.getId())) {
                 stockItemList.set(i, stockItem);
                 return;
             }
